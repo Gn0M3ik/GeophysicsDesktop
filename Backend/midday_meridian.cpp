80f52793a49ec0cfd93ca60639f4398cd084f713
m1R1ng <147048464+m1R1ng@users.noreply.github.com>
2026-03-19 19:12:30 +0300

    Add files via upload
    функция для нахождения полуденного меридиана когда пользователь указывает UTC формат

---
+++ Backend/midday_meridian.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+
+int main(){
+    //int timeLocal;
+    int timeUTC ; 
+
+    setlocale(LC_ALL, "Russian");
+
+    //std::cout <<"введите время в форматее 0011 где 00 часы 11 минуты \n" ;
+    //std::cin  >> timeLocal;
+    std::cout <<"введите время UTS в форматее 0011 где 00 часы 11 минуты \n" ;
+    std::cin >> timeUTC ;
+
+    int hours = timeUTC / 100;   
+    int minutes = timeUTC % 100; 
+    double middayMeridian = 180.0 - (hours * 15.0 + minutes * 0.25);
+    
+    if (hours >24 || minutes >60 )
+    {
+        std::cout << "Ошибка!" << std::endl;
+    }else {
+        
+        // Каждый час = 15, каждая минута = 0.25
+        ;
+        
+        while (middayMeridian > 180.0) middayMeridian -= 360.0;
+        while (middayMeridian < -180.0) middayMeridian += 360.0;
+
+        
+        std::cout << hours << std::endl;
+        std::cout << minutes << std::endl;
+    } 
+
+    std::cout << middayMeridian << std::endl;
+
+    return 0;
+}
