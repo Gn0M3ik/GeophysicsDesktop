dc55ba468050f0ad474874f7e23cd7ea251776ce
m1R1ng <147048464+m1R1ng@users.noreply.github.com>
2026-03-19 19:27:20 +0300

    Update midday_meridian.cpp

--- Backend/midday_meridian.cpp
+++ Backend/midday_meridian.cpp
@@ -3,18 +3,17 @@
 
 
 int main(){
-    //int timeLocal;
     int timeUTC ; 
 
     setlocale(LC_ALL, "Russian");
 
-    //std::cout <<"введите время в форматее 0011 где 00 часы 11 минуты \n" ;
-    //std::cin  >> timeLocal;
     std::cout <<"введите время UTS в форматее 0011 где 00 часы 11 минуты \n" ;
     std::cin >> timeUTC ;
 
     int hours = timeUTC / 100;   
     int minutes = timeUTC % 100; 
+
+    // Каждый час = 15, каждая минута = 0.25
     double middayMeridian = 180.0 - (hours * 15.0 + minutes * 0.25);
     
     if (hours >24 || minutes >60 )
@@ -22,8 +21,7 @@ int main(){
         std::cout << "Ошибка!" << std::endl;
     }else {
         
-        // Каждый час = 15, каждая минута = 0.25
-        ;
+
         
         while (middayMeridian > 180.0) middayMeridian -= 360.0;
         while (middayMeridian < -180.0) middayMeridian += 360.0;
